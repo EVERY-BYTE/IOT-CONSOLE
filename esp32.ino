b5ae5d38bae1f4af492eb01e1ccfe5381dd7a4f7
misdarmanto <misdarmanto1922@gmail.com>
2025-05-24 21:19:52 +0700

    fix bugs arduino code

--- esp32.ino
+++ esp32.ino
@@ -15,20 +15,19 @@ DHT dht(DHT_PIN, DHT_TYPE);
 OneWire oneWire(DS18B20_PIN);
 DallasTemperature ds18b20(&oneWire);
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "POCO M4 PRO"
+#define WIFI_PASSWORD "instiper"
 
 #define FIREBASE_HOST "https://console-iot-default-rtdb.asia-southeast1.firebasedatabase.app/"
 #define FIREBASE_AUTH "AIzaSyC3fjOk-Xs7vN4eqbPbvgKzmhTCjo9DOQM"
 
-#define USER_NAME ""
+#define USER_NAME "ikhsanddt7@gmailcom"
 
-const char* SOIL_SENSOR_ID = "";
+// isi sensor ID-nya supaya bisa kirim data
+const char* SOIL_SENSOR_ID = "223a8e1b-1fb3-428f-b562-338e4b92a8a7";
 const char* LDR_SENSOR_ID  = "";
-const char* DHT_SENSOR_ID = "";
+const char* DHT_SENSOR_ID  = "";
 const char* DS18B20_SENSOR_ID = "";
-
-
 
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 7 * 3600;
@@ -86,34 +85,31 @@ void loop() {
   Serial.println(epochTime);
 
   if (strlen(SOIL_SENSOR_ID) > 0) {
-
     int soilMoistureValue = analogRead(SOIL_MOISTURE_PIN);
 
     FirebaseJson soilJson;
     soilJson.set("value", soilMoistureValue);
     soilJson.set("timestamp", (unsigned long)epochTime);
 
-    if (Firebase.pushJSON(firebaseData, "/" USER_NAME "/deviceData/" SOIL_SENSOR_ID, soilJson)) {
-      Serial.println("Soil Moisture send to Firebase");
+    if (Firebase.pushJSON(firebaseData, String("/") + USER_NAME + "/deviceData/" + SOIL_SENSOR_ID, soilJson)) {
+      Serial.println("Soil Moisture sent to Firebase");
     } else {
       Serial.print("Soil Moisture data failed: ");
       Serial.println(firebaseData.errorReason());
     }
 
     Serial.print("Soil Moisture: ");
-    Serial.print(soilMoistureValue);
+    Serial.println(soilMoistureValue);
   }
 
-
   if (strlen(LDR_SENSOR_ID) > 0) {
-
     int ldrValue = analogRead(LDR_PIN);
 
     FirebaseJson ldrJson;
     ldrJson.set("value", ldrValue);
     ldrJson.set("timestamp", (unsigned long)epochTime);
 
-    if (Firebase.pushJSON(firebaseData, "/" USER_NAME "/deviceData/" LDR_SENSOR_ID, ldrJson)) {
+    if (Firebase.pushJSON(firebaseData, String("/") + USER_NAME + "/deviceData/" + LDR_SENSOR_ID, ldrJson)) {
       Serial.println("LDR data sent.");
     } else {
       Serial.print("LDR data failed: ");
@@ -131,7 +127,7 @@ void loop() {
     tempJson.set("value", temperature);
     tempJson.set("timestamp", (unsigned long)epochTime);
 
-    if (Firebase.pushJSON(firebaseData, "/" + String(USER_NAME) + "/deviceData/" + String(DHT_SENSOR_ID), tempJson)) {
+    if (Firebase.pushJSON(firebaseData, String("/") + USER_NAME + "/deviceData/" + DHT_SENSOR_ID, tempJson)) {
       Serial.println("Temperature sent.");
     } else {
       Serial.print("Temperature failed: ");
@@ -151,7 +147,7 @@ void loop() {
       dsJson.set("value", dsTemp);
       dsJson.set("timestamp", (unsigned long)epochTime);
 
-      if (Firebase.pushJSON(firebaseData, "/" + String(USER_NAME) + "/deviceData/" + String(DS18B20_SENSOR_ID), dsJson)) {
+      if (Firebase.pushJSON(firebaseData, String("/") + USER_NAME + "/deviceData/" + DS18B20_SENSOR_ID, dsJson)) {
         Serial.println("DS18B20 temperature sent.");
       } else {
         Serial.print("DS18B20 failed: ");
